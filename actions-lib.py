d65a9cc58fc972871e7c94536217ee046916f226
madhive-coding <siddarthpradeep@ekyaschools.com>
2026-02-07 10:31:18 +0530

    Update actions-lib.py

--- actions-lib.py
+++ actions-lib.py
@@ -1,1 +1,16 @@
-print()
+'''
+actions-lib library
+'''
+
+from tkinter import *
+from py.game import *
+
+def move_char(Direction, CharName):
+    pass
+
+def movement_action(CharName, Action):
+    pass
+
+def jump_char(CharName, JumpHeight):
+    pass
+
