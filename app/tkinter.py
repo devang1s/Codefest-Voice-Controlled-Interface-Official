9322c93c2328036b9791063d201203faa02cbf33
devang1s <97841937+devang1s@users.noreply.github.com>
2026-02-07 20:44:05 +0530

    Massive commit ig
    ...

---
+++ app/tkinter.py
@@ -0,0 +1,28 @@
+'''
+main logic
+'''
+
+'''
+Imports
+
+pip statements
+
+pip install google-generativeai
+pip install pyttsx3
+pip install pyaudio
+pip install SpeechRecognition
+pip install pyautogui
+'''
+
+from google.generativeai import *
+from pyttsx3 import *
+from pyaudio import *
+from speech_recognition import *
+from pyautogui import *
+import libs.google_genai
+import libs.pyautogui_lib
+import libs.audio_lib
+
+'''
+Inits
+'''
