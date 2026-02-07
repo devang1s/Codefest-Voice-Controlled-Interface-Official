4a20334f9c3eabe815398dec8a76902eca5ed65f
devang1s <97841937+devang1s@users.noreply.github.com>
2026-02-07 12:15:54 +0530

    change of project

---
+++ audio-lib.py
@@ -0,0 +1,42 @@
+'''
+lib for audio functions
+'''
+
+'''
+imports
+
+pip install statements
+
+pip install pyttsx3
+pip install pyaudio
+pip install SpeechRecognition 
+'''
+
+from pyttsx3 import *
+from pyaudio import *
+import speech_recognition as sr
+
+#inits
+
+engine = init()
+r = sr.Recogniser()
+
+#functions
+
+def speech_to_text():
+    with sr.Microphone() as source:
+        print("Adjusting for ambient noise...")
+        r.adjust_for_ambient_noise(source, duration=0.5)
+        print("Speak now...")
+        audio = r.listen(source)
+
+    try:
+        text = r.recognize_google(audio)
+        output = text
+    except sr.UnknownValueError:
+        output = "Audio error - could not understand audio"
+    except sr.RequestError as e:
+        output = f"System error. API error: {e}"
+
+    return output
+
