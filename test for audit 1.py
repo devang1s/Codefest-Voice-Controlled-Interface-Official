85c425ebfee4ccd39ec63289cee24c0187dc91a0
madhive-coding <siddarthpradeep@ekyaschools.com>
2026-02-07 14:14:25 +0530

    Create test for audit 1.py

---
+++ test for audit 1.py
@@ -0,0 +1,49 @@
+'''
+lib for audio functions
+'''
+
+'''
+imports
+
+pip install statements
+
+pip install pyttsx3
+pip install pyaudio
+pip install SpeechRecognition 
+'''
+
+from pyttsx3 import *
+from pyaudio import *
+import speech_recognition as sr
+
+#inits
+
+engine = init()
+r = sr.Recognizer()
+
+#functions
+
+def speech_to_text():
+    with sr.Microphone() as source:
+        print("Adjusting for ambient noise...")
+        r.adjust_for_ambient_noise(source, duration=0.5)
+        print("Speak now...")
+        audio = r.listen(source)
+
+    try:
+        text = r.recognize_google(audio)
+        output = text
+    except sr.UnknownValueError:
+        output = "Audio error - could not understand audio"
+    except sr.RequestError as e:
+        output = f"System error. Error type: API error: {e}"
+
+    return output
+
+
+print(speech_to_text())
+'''def text_to_speech(InputText, EngineVar):
+    EngineVar.say(InputText)
+    EngineVar.runAndWait()
+
+text_to_speech("hello people this is group the tech titans .testing testing testing testing 1  2  3  4  5  6  7  8 9 0    thank you for listening to this sample audio", engine)'''
