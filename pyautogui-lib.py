366eb76fb72994b0a0a251006075c620ea2c5e48
devang1s <97841937+devang1s@users.noreply.github.com>
2026-02-07 16:15:00 +0530

    committed (again)

--- pyautogui-lib.py
+++ pyautogui-lib.py
@@ -38,8 +38,29 @@ def mouse_move(distanceX, distanceY, currentMouseX, currentMouseY, screenWidth, 
     elif newMouseY>(screenHeight/2)-screenHeight:
         newMouseY = (screenHeight/2)-screenHeight
 
-def mouse_other_click(type, distance, currentMouseX, currentMouseY, screenWidth, screenHeight):
-    pass
+    pyautogui.moveTo(newMouseX, newMouseY)
+
+def mouse_other_click(type, distanceX, distanceY, currentMouseX, currentMouseY, screenWidth, screenHeight):
+    if type=='double_click':
+
+        pyautogui.doubleClick()
+
+    if type=='drag_and_hold':
+        
+        newMouseX = currentMouseX + distanceX
+        newMouseY = currentMouseY + distanceY
+
+        if newMouseX>screenWidth/2:
+            newMouseX = screenWidth/2
+        elif newMouseX>(screenWidth/2)-screenWidth:
+            newMouseX = (screenWidth/2)-screenWidth
+
+        if newMouseY>screenHeight/2:
+            newMouseY = screenHeight/2
+        elif newMouseY>(screenHeight/2)-screenHeight:
+            newMouseY = (screenHeight/2)-screenHeight
+
+        pyautogui.dragTo(newMouseX, newMouseY)   
 
 def keyboard_action():
     pass
